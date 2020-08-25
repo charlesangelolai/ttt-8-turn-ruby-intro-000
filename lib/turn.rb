4b1e145260fd07dac6db4c8014f591fc4df2f674
Charles Lai <charlesangeloplai@gmail.com>
2020-08-25 15:50:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,10 @@ def input_to_index(input)
   return input.to_i - 1
 end
 
-def valid_move?(board, index)
-  (index > 8 || index < 0) && position_taken?(board, index) ? false : true
-end
-
 def position_taken?(board, index)
   board[index] != "" || board[index] != " " ? false : true
+end
+
+def valid_move?(board, index)
+  (index > 8 || index < 0) && position_taken?(board, index) ? false : true
 end
