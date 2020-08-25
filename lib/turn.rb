ee9d75ff67c4fc8cce0eb6f681364653e9a7a0ab
Charles Lai <charlesangeloplai@gmail.com>
2020-08-25 16:47:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def move(board, index, token = "X")
 end
 
 def turn(board)
-  print "Please enter 1-9:"
+  puts "Please enter 1-9:"
   input = gets.chomp
   until valid_move?(board, input) == true
     turn(board)
